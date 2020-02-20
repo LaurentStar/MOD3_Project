b3c159269b33206391d7eeddbaa533167a76c994
Laurent_Vinn <laurent.mundell@gmail.com>
2020-02-20 15:01:02 -0500

    Completition

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -1,7 +1,7 @@
 _  = os.path.join('data', 'dataframe_pickle')
 with open(_, 'rb') as file:
     df = pickle.load(file)
-df=df[df['Year'].isin(['2018','2017', '2016', '2015', '2016', '2015', '2014', '2013', '2012', '2011', '2010',])]
+df=df[df['Year'].isin(['2018','2017', '2016', '2015', '2016', '2015', '2014', '2013', '2012', '2011', '2010'])]
 df = df.groupby('State').sum()
 df['Region'] = df.index.map(lambda x: region_maker(x, _REGIONS)) 
 df.sort_values('State', inplace=True)
@@ -15,3 +15,10 @@ population_df.set_index('States', inplace=True)
 
 df = pd.concat([df, population_df.sum(axis = 1)], sort=False, axis=1)
 df.rename(columns={0: "Population_Sum"}, inplace=True)
+
+
+#Load data for hypothesis 2----------------------------------------------------------
+_  = os.path.join('data', 'dataframe_pickle')
+with open(_, 'rb') as file:
+    df2 = pickle.load(file)
+df2=df2[df2['Year'].isin(['2018','2017', '2016', '2015', '2016', '2015', '2014', '2013', '2012', '2011', '2010',])]
---
+++ scripts/hypothesis_2.py
@@ -0,0 +1,20 @@
+formula = 'Carbon_Emissions ~ C(State) + C(Year) + C(State) * C(Year)' # To be continued
+model = ols(formula, df2).fit()
+model.summary() 
+print(table)
+
+
+# se = df[df['Region'] == 'south_east']['Carbon_Emissions'].to_numpy()
+# ne = df[df['Region'] == 'north_east']['Carbon_Emissions'].to_numpy()
+# mw = df[df['Region'] == 'mid_west']['Carbon_Emissions'].to_numpy()
+# wc = df[df['Region'] == 'west_coast']['Carbon_Emissions'].to_numpy()
+
+
+# regions = ['se', 'ne', 'mw', 'wc', 'sw']
+# nCk_regions = list(combinations(regions, 2))
+# hypothesis = []
+
+# for region_pair in nCk_regions:
+#     a = region_pair[0]
+#     b = region_pair[1]
+#     exec(f"hypothesis.append((stats.ttest_ind({a}, {b}), {region_pair}))")
