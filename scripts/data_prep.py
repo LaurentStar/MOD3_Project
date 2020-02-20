9f224c937ca948477b683f0811385955a7f907ee
Laurent_Vinn <laurent.mundell@gmail.com>
2020-02-20 16:41:09 -0500

    Graphs

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -1,20 +1,20 @@
 _  = os.path.join('data', 'dataframe_pickle')
 with open(_, 'rb') as file:
     df = pickle.load(file)
-df=df[df['Year'].isin(['2018','2017', '2016', '2015', '2016', '2015', '2014', '2013', '2012', '2011', '2010'])]
+df=df[df['Year'].isin(['2017', '2016', '2015', '2016', '2015', '2014', '2013', '2012', '2011', '2010'])]
 df = df.groupby('State').sum()
 df['Region'] = df.index.map(lambda x: region_maker(x, _REGIONS)) 
 df.sort_values('State', inplace=True)
 
 population_df = load_population_data()
 population_df = population_df.iloc[5:, ]
-population_df.drop(['Census', 'Estimates Base', 2019], axis=1, inplace=True)
+population_df.drop(['Census', 'Estimates Base', 2019, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2018], axis=1, inplace=True)
 population_df.drop(16, axis=0, inplace=True)
 population_df.sort_values('States', inplace=True)
 population_df.set_index('States', inplace=True)
 
 df = pd.concat([df, population_df.sum(axis = 1)], sort=False, axis=1)
-df.rename(columns={0: "Population_Sum"}, inplace=True)
+df.rename(columns={0: "Population_2017"}, inplace=True)
 
 
 #Load data for hypothesis 2----------------------------------------------------------
